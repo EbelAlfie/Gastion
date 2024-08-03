ebeade1e841a609496ef99e4ed7794622580860a
EbelAlfie <davis.onde.wei@gmail.com>
2024-08-04 02:36:47 +0700

    create button defaults

--- app/src/main/java/com/example/gastion/ui/theme/components/Button.kt
+++ app/src/main/java/com/example/gastion/ui/theme/components/Button.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
@@ -19,36 +20,53 @@ object PositiveButtonDefaults {
   @Composable
   fun buttonColor(): ButtonColors =
     ButtonDefaults.buttonColors(
-      containerColor = Color.Unspecified,
-  contentColor: Color = Color.Unspecified,
+      containerColor = Red,
+      contentColor = Color.White,
     )
 }
 
 object NegativeButtonDefaults {
   val shape: Shape = RoundedCornerShape(5.dp)
   val border: BorderStroke = BorderStroke(1.dp, Red)
-  val contentColor: Color = Color.White
-  val textColor: Color = Red
+
+  @Composable
+  fun buttonColor(): ButtonColors =
+    ButtonDefaults.buttonColors(
+      containerColor = Red,
+      contentColor = Color.White,
+    )
 }
 
 @Composable
 fun PrimaryGasButton(
+  modifier: Modifier = Modifier,
   label: String,
   onClick: () -> Unit,
 ) {
-  Button(onClick = onClick) {
+  val buttonDefaults = PositiveButtonDefaults
+  Button(
+    modifier = modifier,
+    shape = buttonDefaults.shape,
+    colors = buttonDefaults.buttonColor(),
+    border = buttonDefaults.border,
+    onClick = onClick
+  ) {
     Text(label)
   }
 }
 
 @Composable
 fun SecondaryGasButton(
+  modifier: Modifier = Modifier,
   label: String,
   onClick: () -> Unit,
 ) {
   val buttonDefaults = NegativeButtonDefaults
   Button(
+    modifier = modifier,
     shape = buttonDefaults.shape,
+    colors = buttonDefaults.buttonColor(),
+    border = buttonDefaults.border,
     onClick = onClick
   ) {
     Text(label)
