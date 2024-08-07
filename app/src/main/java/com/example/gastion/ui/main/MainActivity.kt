fb3796594cee74e70326ec484647cce449883091
EbelAlfie <davis.onde.wei@gmail.com>
2024-08-08 02:41:53 +0700

    fixing

--- app/src/main/java/com/example/gastion/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/gastion/ui/main/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.gastion.ui.main
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import com.example.gastion.ui.core.BaseActivity
-import com.example.gastion.ui.core.BaseUiDelegate
-import com.example.gastion.ui.core.BaseUiEvents
 import com.example.gastion.ui.core.Screen
 import com.example.gastion.ui.main.MainScreens.Login
 import com.example.gastion.ui.main.MainScreens.Maps
@@ -20,7 +14,7 @@ import com.example.gastion.ui.theme.GastionTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<MainScreens, BaseUiEvents>() {
+class MainActivity : BaseActivity<MainScreens>() {
 
   override val viewModel: MainViewModel by viewModels()
 
